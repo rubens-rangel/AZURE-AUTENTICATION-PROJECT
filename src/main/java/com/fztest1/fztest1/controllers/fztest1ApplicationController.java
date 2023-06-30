ad9353b924a75d88e1e81bd1ffe87a77fe6f0c4b
Rubens Rangel <rubens.rangel@b2wdigital.com>
2023-06-30 15:35:20 -0300

    teste 11

--- src/main/java/com/fztest1/fztest1/controllers/fztest1ApplicationController.java
+++ src/main/java/com/fztest1/fztest1/controllers/fztest1ApplicationController.java
@@ -1,41 +1,28 @@
 package com.fztest1.fztest1.controllers;
 
-import org.springframework.util.MultiValueMap;
+import com.fztest1.fztest1.dto.SAMLResponseDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+
+
 @RestController
 public class fztest1ApplicationController {
 
-//	private static final Logger LOGGER = LoggerFactory.getLogger(AuthADCallback.class);
 
-	@PostMapping("/saml/sso")
-	public String samlSSO(@RequestBody MultiValueMap<String, String> paramMap) {
+	@PostMapping("/saml/callback")
+	public void samlCallback(@RequestBody SAMLResponseDto samlResponseDto, HttpServletRequest request) {
+		// Decodificar o SAMLResponse
+		byte[] samlResponseBytes = Base64.getDecoder().decode(samlResponseDto.getSamlResponse());
+		String samlResponseXml = new String(samlResponseBytes);
+		System.out.println(samlResponseXml);
 
-//		if(authentication != null) {
-//			if (LOGGER.isDebugEnabled()) {
-//			}
-//			LOGGER.info("Auth Name: {}", authentication.getName());
-//			LOGGER.info("Auth Credentials: {}", authentication.getCredentials());
-//			LOGGER.info("Auth Principal: {}", authentication.getPrincipal());
-//		}
-//		if (samlResponse != null && !samlResponse.isEmpty()) {
-//			// Decodificar o token SAMLResponse e extrair informações relevantes
-//			// Realizar validações e lógica de negócio necessárias
-//			// Redirecionar para a página principal ou rota desejada
-//			LOGGER.debug("Token Recebido do AD {}", samlResponse);
-//			return "token";
-//		} else {
-//			// Tratar caso o token de autorização não seja válido ou esteja ausente
-//			LOGGER.debug("Token Recebido do AD com erro {}", samlResponse);
-//			return "Erro";
-//		}
-//	}
-		for (int i = 0; i < paramMap.size(); i ++){
-			System.out.println("value" + paramMap  );
-		}
-
-		return "recebido";
 	}
 }
+
+
+
+
---
+++ src/main/java/com/fztest1/fztest1/dto/SAMLResponseDto.java
@@ -0,0 +1,13 @@
+package com.fztest1.fztest1.dto;
+
+public class SAMLResponseDto {
+    private String samlResponse;
+
+    public String getSamlResponse() {
+        return samlResponse;
+    }
+
+    public void setSamlResponse(String samlResponse) {
+        this.samlResponse = samlResponse;
+    }
+}
